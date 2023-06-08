c902cc7a210f222526c9b760940e4d1ee62e7347
MichalGomola <michal.gomola.89@gmail.com>
2023-06-08 19:51:14 +0200

    SumCharOccu task done

---
+++ AlgorythmicaTasks/src/main/java/org/mg/intermediate/SumCharOccurence.java
@@ -0,0 +1,24 @@
+package org.mg.intermediate;
+
+public class SumCharOccurence {
+
+    public static String count(final String str) {
+        StringBuilder result = new StringBuilder();
+        String[] letters = str.split("");
+        int counter = 0;
+        String letter = letters[0];
+
+        for (String s: letters) {
+            if (letter.equals(s)) {
+                counter++;
+            } else {
+                result.append(counter).append(letter);
+                counter = 1;
+                letter = s;
+            }
+        }
+        result.append(counter).append(letter);
+
+        return result.toString();
+    }
+}
